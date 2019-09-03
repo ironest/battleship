bea6de78ecfe40ba19748cd9528b3f9f35b334ec
ironest <ironest@gmail.com>
2019-09-03 10:48:50 +1000

    Added method to randomly place a ship on a board

--- files/util.rb
+++ files/util.rb
@@ -13,3 +13,47 @@ def init_board
     return board
 
 end
+
+
+
+def random_place_ship(size, board)
+
+    fit = false
+    until fit do
+
+        max_x = 9
+        max_y = 9
+        newBoard = Marshal.load( Marshal.dump(board) )
+
+        if rand(0..1) == 0
+            orientation = :vertical
+            max_x = 9 - size
+        else
+            orientation = :horizontal
+            max_y = 9 - size
+        end
+
+        x = rand(0..max_x)
+        y = rand(0..max_y)
+
+        for c in 1..size
+            if newBoard[x][y] == 0
+                fit = true
+                newBoard[x][y] = size
+            else
+                fit = false
+                break
+            end
+
+            if orientation == :vertical
+                x+=1
+            else
+                y+=1
+            end
+
+        end
+    end
+
+    return newBoard
+
+end
