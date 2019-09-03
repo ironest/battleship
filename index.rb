1dc872630e3dd3e0f5c6d38581fdbcc701728ea5
ironest <ironest@gmail.com>
2019-09-03 12:35:43 +1000

    Added more instructions (print) for a user

--- index.rb
+++ index.rb
@@ -10,12 +10,6 @@ player = "Ricky"
 player_board = init_board
 enemy_board  = init_board
 
-# 5.times do |x|
-#     5.times do |y|
-#         player_board[x*2][y*2] = rand(0..4)
-#     end
-# end
-
 ship_list.each do |ship_size|
     player_board = random_place_ship(ship_size, player_board)
     enemy_board = random_place_ship(ship_size, enemy_board)
@@ -35,15 +29,23 @@ until is_board_clear(player_board) or is_board_clear(enemy_board)
             print "Where do you want to shoot? (e.g. B5) "
             coordinates = gets.chomp
             break if validate_coordinates(coordinates)
+            print "Wrong coordinates... try again"
+            sleep 1
+            system("clear")
+            render_boards(player_board, enemy_board)
         end
         
         #implement method to check input
-        x = parse_coordinates(coordinates)[0]
-        y = parse_coordinates(coordinates)[1]
+        xy = parse_coordinates(coordinates)
+
+        update_board_after_shot(enemy_board, xy)
+
     else
         puts "\nIt's computer turn!"
+        update_board_after_shot(player_board, [0, 0])
     end
 
+    turn_counter += 1
     sleep 1
 end
 
