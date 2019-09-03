2e1ebe2d2ef66eff1030f8129ca4283e17de5511
ironest <ironest@gmail.com>
2019-09-03 10:50:51 +1000

    Autonomous placement of ships

--- index.rb
+++ index.rb
@@ -1,6 +1,8 @@
 require_relative "./files/util"
 require_relative "./files/graphic"
 
+ship_list = [1, 1, 1, 2, 2, 2, 3, 3, 4]
+
 # print "\nEnter your name: "
 # player = gets.chomp
 
@@ -13,18 +15,14 @@ enemy_board  = init_board
 #     end
 # end
 
-player_board[3][3] = 4
-player_board[3][4] = 4
-player_board[3][5] = 4
-player_board[3][6] = 4
-
-player_board[2][3] = 3
-player_board[2][4] = 3
-player_board[2][5] = 3
-
+ship_list.each do |ship_size|
+    player_board = random_place_ship(ship_size, player_board)
+    enemy_board = random_place_ship(ship_size, enemy_board)
+end
 
 render_boards(player_board, enemy_board)
 
 print "Where do you want to shoot? (e.g. B5)"
 coordinates = gets.chomp
 #implement method to check input
+
